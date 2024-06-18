4607b747999de0a7592695e54683c8d447c364d0
Brian Li <brnli7@gmail.com>
2024-06-18 15:14:25 -0700

    Fix faucet cmd

--- publish-check.sh
+++ publish-check.sh
@@ -9,7 +9,12 @@ CHECK_ADDRESS="${CHECK_ADDRESS}"
 echo "PUBLISH_COMMAND: $PUBLISH_COMMAND"
 echo "CHECK_ADDRESS: $CHECK_ADDRESS"
 
-http_code=$(curl --write-out '%{http_code}' --silent --output /dev/null https://fullnode.devnet.aptoslabs.com/v1/accounts/$CHECK_ADDRESS/resource/0x1::code::PackageRegistry)
+http_code=$(
+  curl --write-out '%{http_code}' \
+       --silent \
+       --output /dev/null \
+       https://fullnode.devnet.aptoslabs.com/v1/accounts/$CHECK_ADDRESS/resource/0x1::code::PackageRegistry
+)
 
 if [ $http_code -eq 200 ]; then
   echo "Package is already published at $CHECK_ADDRESS"
@@ -17,6 +22,7 @@ elif [ $http_code -eq 404 ]; then
   echo "Package is not published. Running publish command."
   aptos account fund-with-faucet \
     --account="$CHECK_ADDRESS" \
+    --url="https://fullnode.devnet.aptoslabs.com/" \
     --faucet-url="https://faucet.devnet.aptoslabs.com"
   eval "$PUBLISH_COMMAND"
 else
