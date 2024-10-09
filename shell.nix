4ced5566dc0df0c82dbb2accd667deb18a2bf277
Brian Li <brnli7@gmail.com>
2024-10-09 10:24:13 -0700

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -2,11 +2,6 @@ with import <nixpkgs> { };
 
 pkgs.mkShell {
   buildInputs = [
-    openssl_1_1
     (callPackage ./aptos.nix { })
   ];
-
-  shellHook = ''
-    export LD_LIBRARY_PATH=${openssl_1_1.out}/lib:$LD_LIBRARY_PATH
-  '';
 }
